0c39291f397aa9ffa5fef0d2a1e4578c8ed0f5bf
uffjatt <65695620+uffjatt@users.noreply.github.com>
2020-08-20 14:44:15 +0530

    Update CMDS.py

--- tg_bot/modules/CMDS.py
+++ tg_bot/modules/CMDS.py
@@ -16,7 +16,7 @@ def media_telegraph(bot: Bot, update: Update):
 
 @run_async
 def post_telegraph(bot: Bot, update: Update, args: List[str]):
-    short_name = "Created By @D_ar_k_Angel 😬"
+    short_name = "Created By @Gillz_13 😬"
     msg = update.effective_message # type: Optional[Message]
     telegraph = Telegraph()
     r = telegraph.create_account(short_name=short_name)
@@ -34,11 +34,11 @@ def post_telegraph(bot: Bot, update: Update, args: List[str]):
 
 __help__ = """
 ○ My Name : *Filter Bot*
-○ Creator : [D_ar_k_Angel](http://telegram.dog/D_ar_k_Angel)
+○ Creator : [GILLZ_13](http://telegram.dog/GILLZ_13)
 ○ Credits : `Everyone in this journey`
 ○ Language : Python3
-○ Library : [Pyrogram asyncio 0.16.1](https://docs.pyrogram.org/)
-○ Source Code : 👉 [Click Here](https://github.com/DarkAngelTG/Filter-Bot)
+○ Library : [Leech_botz](https://t.me/joinchat/SyRE_E-X0awt7OVfFwSsUw)
+○ Source Code : 👉 [Click Here](https://t.me/Filmyflx)
 ○ Server : Heroku
 ○ Build Status : V4 `[+0.4]`
 """
